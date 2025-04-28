eeb5555be273b3e8e6432afa8e6b1730a2a1fcd8
TiaMeds 4 <shreeraksha@tiameds.com>
2025-04-28 13:59:16 +0530

    Updated

--- src/app/healthcare/page.tsx
+++ src/app/healthcare/page.tsx
@@ -6,31 +6,58 @@ import Image from "next/image";
 const Page = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-between pt-24 bg-[url(/HealthcareBg.svg)] h-[500px] md:h-[880px] bg-no-repeat bg-cover bg-[position:left] md:bg-[position:right]"></div>
+      <div
+        className="flex flex-col md:flex-row justify-between pt-24 min-w-0 h-[917px] md:h-[880px] bg-no-repeat bg-cover bg-[position:left] 
+  bg-[url('/HealthcareMobileBg.svg')] md:bg-[url('/HealthcareBg.svg')]"
+      ></div>
 
-      <main className="flex flex-col items-center justify-center mx-10 my-20 gap-10">
+      <main className="flex flex-col items-center justify-center m-5 space-y-5 sm:m-20 sm:gap-10">
+        
         <div className="relative group w-full h-[692px] overflow-hidden cursor-pointer">
-          <Image src="/ShplImg.svg" alt="Image" fill className="object-cover" />
+          <div className="hidden sm:block w-full h-full relative">
+            <Image
+              src="/ShplImg.svg"
+              alt="Image"
+              fill
+              className="object-cover"
+            />
 
-          <div className="absolute top-8 bottom-8 left-8 p-10 space-y-10 text-white h-auto w-[596px] bg-black/30 backdrop-blur-sm flex flex-col opacity-0 group-hover:opacity-100 transition-all duration-300">
-            <p className="font-medium text-4xl font-serif">
-              Sudhanand Healthcare Solutions Pvt. Ltd.
-            </p>
-            <p className="font-normal text-2xl max-w-lg">
-              Cureplus Hospitals is a growing network across Mysore,
-              Chamarajanagar & Kodagu, led by the 40-bed NABH-accredited
-              Cureplus Disha Hospital in Mysuru. The network includes 20-bed
-              hospitals in T. Narasipura, Bherya, H.D. Kote, Halli Mysuru,
-              Hosur, Ramapura & Terakanambi, plus facilities in Santhemarahalli,
-              Kushalnagar & Bettadapura. Offering affordable, quality care with
-              modern diagnostics, Cureplus combines clinical excellence with
-              compassion across urban & rural communities.
-            </p>
+            <div className="absolute top-8 bottom-8 left-8 p-10 space-y-10 text-white h-auto w-[596px] bg-black/30 backdrop-blur-sm flex flex-col opacity-0 group-hover:opacity-100 transition-all duration-300">
+              <p className="font-medium text-4xl font-serif">
+                Sudhanand Healthcare Solutions Pvt. Ltd.
+              </p>
+              <p className="font-normal text-2xl max-w-lg">
+                Cureplus Hospitals is a growing network across Mysore,
+                Chamarajanagar & Kodagu, led by the 40-bed NABH-accredited
+                Cureplus Disha Hospital in Mysuru. The network includes 20-bed
+                hospitals in T. Narasipura, Bherya, H.D. Kote, Halli Mysuru,
+                Hosur, Ramapura & Terakanambi, plus facilities in
+                Santhemarahalli, Kushalnagar & Bettadapura. Offering affordable,
+                quality care with modern diagnostics, Cureplus combines clinical
+                excellence with compassion across urban & rural communities.
+              </p>
+            </div>
+          </div>
+
+          <div className="block sm:hidden w-[348px] h-[800px] relative">
+            <Image
+              src="/ShplMobileImg.svg"
+              alt="Mobile Image"
+              fill
+              className="object-cover"
+            />
           </div>
         </div>
 
-        <div className="relative group w-full h-[692px] overflow-hidden  cursor-pointer">
-          <Image src="/SpplImg.svg" alt="Image" fill className="object-cover" />
+        <div className="relative group w-full h-[692px] overflow-hidden cursor-pointer">
+
+        <div className="hidden sm:block w-full h-full relative">
+            <Image
+              src="/SpplImg.svg"
+              alt="Image"
+              fill
+              className="object-cover"
+            />
 
           <div className="absolute top-8 bottom-8 right-8 p-10 space-y-10 text-white h-auto w-[596px] bg-black/30 backdrop-blur-sm flex flex-col opacity-0 group-hover:opacity-100 transition-all duration-300">
             <p className="font-medium text-4xl font-serif">
@@ -47,6 +74,18 @@ const Page = () => {
               name in the pharmaceutical industry.
             </p>
           </div>
+          </div>
+
+
+          <div className="block sm:hidden w-[348px] h-[800px] relative">
+            <Image
+              src="/SpplMobileImg.svg"
+              alt="Mobile Image"
+              fill
+              className="object-cover"
+            />
+          </div>
+
         </div>
       </main>
     </>
